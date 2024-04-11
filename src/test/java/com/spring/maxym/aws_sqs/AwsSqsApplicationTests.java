d9872d5cfd4efe7b636a0011b859e5d98606e79c
Maxym <so.unique.december@gmail.com>
2024-04-11 19:47:23 +0300

    init commit

---
+++ src/test/java/com/spring/maxym/aws_sqs/AwsSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.maxym.aws_sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSqsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
